aff9b44e5a2b564e7621869cae5741b37de8de74
hadieyhaliemy <hadieyhaliemy>
2022-06-03 21:56:21 +0900

    map 1

--- app/src/main/java/com/example/mindtrip/MainActivity.kt
+++ app/src/main/java/com/example/mindtrip/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.example.mindtrip
 
+import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mindtrip.databinding.ActSelectBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActSelectBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActSelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+
+
+    }
+
+    fun handleSendText(intent: Intent) {
+        val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (sharedText != null) {
+            System.out.println(sharedText);
+
+            val coder = Geocoder(this);
+            val address: List<Address>
+
+            try {
+
+                address = coder.getFromLocationName(sharedText, 5);
+                val location = address.get(0);
+                val lat = location.getLatitude()
+                val lng = location.getLongitude();
+
+            } catch (e: IOException) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private fun init() {
+        with(binding) {
+            cafebtn.setOnClickListener {
+                val gmmIntentUri = Uri.parse("geo:0,0?q=카페")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+
+                startActivity(mapIntent)
+//                val intent = getIntent()
+                val action = mapIntent.getAction()
+                val type = mapIntent.getType()
+
+                if (Intent.ACTION_SEND.equals(action) && type != null) {
+                    if ("text/plain".equals(type)) {
+                        handleSendText(mapIntent); // Handle text being sent
+                    }
+                }
+            }
+            parkbtn.setOnClickListener {
+                val gmmIntentUri = Uri.parse("geo:0,0?q=공원")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                startActivity(mapIntent)
+                val intent = getIntent()
+                val action = intent.getAction()
+                val type = intent.getType()
+
+                if (Intent.ACTION_SEND.equals(action) && type != null) {
+                    if ("text/plain".equals(type)) {
+                        handleSendText(intent); // Handle text being sent
+                    }
+                }
+            }
+            themebtn.setOnClickListener {
+                val gmmIntentUri = Uri.parse("geo:0,0?q=테마파크")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                startActivity(mapIntent)
+                val intent = getIntent()
+                val action = intent.getAction()
+                val type = intent.getType()
+
+                if (Intent.ACTION_SEND.equals(action) && type != null) {
+                    if ("text/plain".equals(type)) {
+                        handleSendText(intent); // Handle text being sent
+                    }
+                }
+            }
+            landmarkbtn.setOnClickListener {
+                val gmmIntentUri = Uri.parse("geo:0,0?q=관광지")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                startActivity(mapIntent)
+                val intent = getIntent()
+                val action = intent.getAction()
+                val type = intent.getType()
+
+                if (Intent.ACTION_SEND.equals(action) && type != null) {
+                    if ("text/plain".equals(type)) {
+                        handleSendText(intent); // Handle text being sent
+                    }
+                }
+            }
+        }
     }
 }
