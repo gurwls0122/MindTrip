6591bc1b853450ef4c33f80569c12279131c0868
SUB1NJ0 <sub2nnj5@gmail.com>
2022-06-05 20:19:47 +0900

    FEAT: change buttons to img buttons in DiaryWriteActivity

--- app/src/main/java/com/example/mindtrip/DiaryData.kt
+++ app/src/main/java/com/example/mindtrip/DiaryData.kt
@@ -1,5 +1,5 @@
 package com.example.mindtrip
 
-data class DiaryData(var title:String, var year: Int, var month:Int, var date:Int, var content:String){
+data class DiaryData(var title:String, var year: Int, var month:Int, var day:Int, var content:String){
 
 }
